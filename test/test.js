656f6f08fa55facd8f0dc733ea5ce8daa917ea34
Pulido V. P. E <pulidovpe.dev@gmail.com>
2019-10-07 23:28:58 -0400

    feat: Update mocha test

--- test/test.js
+++ test/test.js
@@ -3,43 +3,43 @@
 const assert = require('assert');
 const request = require('supertest');
 const app = require('../bundle/bin/www');
+const Fakerator = require("fakerator");
 
 const server = request("http://localhost:8000");
 
-describe('Index test', function() {
+const fakerator = Fakerator("es-ES");
+const fakeEmail = fakerator.internet.email();
+console.log(fakeEmail);
+
+describe('API Unit Testing', function() {
   describe('GET', function(){
-    it('should serve json on index', function(done){
+    it('Should serve json on index', function(done){
       server.get('/')
-        .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
-  });
-});
-describe('Register test', function() {
-  describe('POST', function(){
-    it('should serve json on index', function(done){
+
+    it('Should insert a user', function(done){
       let user = {
-        email: "test@email.com",
+        email: fakeEmail,
         password: "123456",
         fullname: "Test User"
       }
       server.post('/registerUser')
         .send(user)
+        .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(201, done)
     });
-  });
-});
-describe('Login test', function() {
-  describe('POST', function(){
-    it('should serve json on index', function(done){
+
+    it('Should login with the new user', function(done){
       let user = {
-        email: "test@email.com",
+        email: fakeEmail,
         password: "123456"
       }
       server.post('/loginUser')
         .send(user)
+        .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200, done)
     });
